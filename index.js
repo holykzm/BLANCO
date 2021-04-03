eca12412a52a8fabf02f49bde0c60d2e7174515f
holykzm <64673947+holykzm@users.noreply.github.com>
2021-04-03 23:38:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ $(function () {
       min: [10,00], //最小時間
       max: [19,00] //最大時間
       });
+      //タイムピッカー用スクリプト2
+      $("#timepicker2").pickatime({
+      format: "HH:i",
+      interval: 5, //時間間隔（分で指定）
+      min: [10,00], //最小時間
+      max: [19,00] //最大時間
+      });
     });
 
    
@@ -22,6 +29,7 @@ $(function () {
     $('form').submit(function () {
         var date = $('#datepicker').val();
         var time = $('#timepicker').val();
+        var time2 = $('#timepicker2').val();
         var names = $('input[name="names"]').val();
         var staff = $('input[name="s-name"]:checked').val();
 
@@ -32,7 +40,7 @@ $(function () {
         var memo = $('input[name="memo"]').val();
 
 
-        var msg = `希望日：${date}\n\n時間：${time}\n\n氏名：${names}様\n\n担当指名：${staff}\n\nメニュー：${menu}\n\n概要：${memo}\n\n\n送信ありがとうございます！\n返信が来るまでしばらくお待ちください♪`;
+        var msg = `希望日：${date}\n\n時間：${time}から${time2}までの間\n\n氏名：${names}様\n\n担当指名：${staff}\n\nメニュー：${menu}\n\n概要：${memo}\n\n\n送信ありがとうございます！\n返信が来るまでしばらくお待ちください♪`;
         sendText(msg);
 
         return false;
